b2db99b9967b97a814e90dfe25972176a332d085
Asus <Asus@DESKTOP-P9MTI9L>
2022-11-16 19:00:32 +0300

    ..

---
+++ kasım1/src/kasım1/arkla.java
@@ -0,0 +1,36 @@
+package kasým1;
+
+import java.util.HashMap;
+
+public class arkla { 
+	 public static void main(String[] args) {
+		  
+		 HashMap hash  = new HashMap();
+		 hash.put(1, "Fernando Muslera");
+		 hash.put(5, "Lucas Torreria");
+		 hash.put(7, "Kerem Aktürkoðlu");
+		 hash.put(10,"Dries Mertens");
+		 hash.put(13, "Emre Taþdemir");
+		 hash.put(22, "Victor Nelsson");
+		 hash.put(26, "Milot Rashica");
+		 hash.put(27, "Sergio Oliviera");
+		 hash.put(42, "Abdülkerim Bardakçý");
+		 hash.put(93, "Sacha Boey");
+		 hash.put(99, "Mauro Icardi");
+	
+		 System.out.println(hash.get(1));
+		 System.out.println(hash.get(5));
+		 System.out.println(hash.get(7));
+		 System.out.println(hash.get(10));
+		 System.out.println(hash.get(13));
+		 System.out.println(hash.get(22));
+		 System.out.println(hash.get(26));
+		 System.out.println(hash.get(27));
+		 System.out.println(hash.get(42));
+		 System.out.println(hash.get(93));
+		 System.out.println(hash.get(99));
+		  
+		  
+	}
+
+}
